e1b7415b32a5aa714e68bb35ef6f70e1f9b01f96
Martim Baltazar <martimnunesbaltazar@gmail.com>
2025-04-21 11:57:19 +0100

    feat: create logo component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import { UserProvider } from '@auth0/nextjs-auth0/client'
+import { DM_Sans, DM_Serif_Display} from '@next/font/google';
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
